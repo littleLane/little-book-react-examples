c534fd09ae0d792d720a2c4ab7e2502852d0d8ad
lizz1 <lizz1@guahao.com>
2018-04-08 16:55:05 +0800

    修改 demo02 测试案例

--- hello-react/src/test/Demo02.test.js
+++ hello-react/src/test/Demo02.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Demo01 from '../components/Demo01';
+import Demo02 from '../components/Demo02';
 
 it('renders ok', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Demo01 />, div);
+  ReactDOM.render(<Demo02 />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
